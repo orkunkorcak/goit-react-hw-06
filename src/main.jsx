b66470b61e69cfe7a29996a2058fc5a33cbf1ce0
your_name <orkunkorcak@gmail.com>
2025-03-29 16:11:17 +0300

    hw finished

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
-import { StrictMode } from 'react'
-import './index.css'
-import App from './App.jsx'
-import { Provider } from 'react-redux'
-import { store } from './redux/store.js'
+import { createRoot } from "react-dom/client"; // Eksik olan import
+import "./index.css";
+import App from "./components/app/App.jsx";
+import { Provider } from "react-redux";
+import { store } from "./redux/store.js";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <App />
-  </Provider>,
-)
+  </Provider>
+);
